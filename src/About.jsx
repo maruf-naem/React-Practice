a242c444362b0473b932028d980f0c4823fdd4df
Maruf Ahmed Naem <naema4604@gmail.com>
2026-07-09 17:39:03 +0600

    this

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,50 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 function About() {
-  async function studentData() {
-    const url = "http://localhost:3000/students";
-    let response = await fetch(url);
-    response = await response.json();
-    console.log(response);
-  }
+  const [data, setData] = useState([])
 
+  async function userData() {
+    const url = "http://localhost:3000/students"
+    let res = await fetch(url)
+    res = await res.json()
+    setData(res)
+  }
   useEffect(() => {
-    studentData();
-  });
+    userData()
+  }, [])
+
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Department</td>
+            <td>CGPA</td>
+            <td>E-mail</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item)=>{
+              return(
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.department}</td>
+                  <td>{item.cgpa}</td>
+                  <td>{item.email}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
-export default About;
+export default About
